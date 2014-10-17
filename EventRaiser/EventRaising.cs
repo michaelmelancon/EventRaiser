49bd3b588dbf7e1ce82b805b06350b97c6550fa5
Michael Melancon <michaelmelancon@gmail.com>
2014-10-17 08:38:49 -0500

    Added license and documentation.

--- EventRaiser/EventRaising.cs
+++ EventRaiser/EventRaising.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+   Copyright 2014 Michael Melancon
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,12 +23,13 @@ namespace EventRaiser
     public static class EventRaising
     {
         /// <summary>
-        /// Be careful with this one. It should only be used for EventHandler style delegates (Action&lt;Object,EventArgs&gt;).
+        /// Returns a new <see cref="System.EventHandler{T}"/> equivilent to a delegate compatible with the event handler pattern, <c>void Method(object sender, EventArgs arg)</c>. i.e. <see cref="System.ComponentModel.PropertyChangedEventHandler"/>.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="handler"></param>
-        /// <exception cref="System.ArgumentException"></exception>
-        /// <returns></returns>
+        /// <remarks>The new delegate will have an equivilent invocation list, it will not be simply wrapped.</remarks>
+        /// <typeparam name="T">The type of <see cref="System.EventArgs"/> used by the handler.</typeparam>
+        /// <param name="handler">a delegate compatible with the event handler pattern.</param>
+        /// <exception cref="System.ArgumentException">If the delegate is not compatible with the event handler pattern.</exception>
+        /// <returns>a new <see cref="System.EventHandler{T}"/> equivilent to <paramref name="handler"/></returns>
         public static EventHandler<T> ToHandlerOf<T>(this Delegate handler) where T : EventArgs
         {
             if (handler == null)
@@ -30,49 +46,49 @@ namespace EventRaiser
         }
 
         /// <summary>
-        /// 
+        /// Returns a new <see cref="System.EventHandler{T}"/> equivilent to a <see cref="System.EventHandler"/>.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="handler"></param>
-        /// <returns></returns>
+        /// <typeparam name="T">The type of <see cref="System.EventArgs"/> used by the handler.</typeparam>
+        /// <param name="handler">the event handler to convert.</param>
+        /// <returns>a new <see cref="System.EventHandler{T}"/> equivilent to <paramref name="handler"/></returns>
         public static EventHandler<T> ToHandlerOf<T>(this EventHandler handler) where T : EventArgs
         {
             return ((Delegate)handler).ToHandlerOf<T>();
         }
 
         /// <summary>
-        /// 
+        /// Returns a new <see cref="System.EventHandler{T}"/> equivilent to a <see cref="System.EventHandler{TS}"/>, providing contravariance support.
         /// </summary>
-        /// <typeparam name="S"></typeparam>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="handler"></param>
-        /// <returns></returns>
-        public static EventHandler<T> ToHandlerOf<S, T>(this EventHandler<S> handler)
-            where S : EventArgs
-            where T : S
+        /// <typeparam name="TS">The type of <see cref="System.EventArgs"/> used by the input handler.</typeparam>
+        /// <typeparam name="T">The type of <see cref="System.EventArgs"/> used by the output handler.</typeparam>
+        /// <param name="handler">the event handler to convert.</param>
+        /// <returns>a new <see cref="System.EventHandler{T}"/> equivilent to <paramref name="handler"/></returns>
+        public static EventHandler<T> ToHandlerOf<TS, T>(this EventHandler<TS> handler)
+            where TS : EventArgs
+            where T : TS
         {
             return handler.ToHandlerOf<T>();
         }
 
         /// <summary>
-        /// 
+        /// Converts an EventHandler to an EventHandler&lt;EventArgs&gt;.
         /// </summary>
-        /// <param name="handler"></param>
-        /// <returns></returns>
+        /// <param name="handler">the event handler to convert.</param>
+        /// <returns>An equivilent EventHandler&lt;EventArgs&gt;</returns>
         public static EventHandler<EventArgs> ToGeneric(this EventHandler handler)
         {
             return handler.ToHandlerOf<EventArgs>();
         }
 
         /// <summary>
-        /// 
+        /// Combines a sequence of <see cref="System.EventHandler{T}"/> into a multicast <see cref="System.EventHandler{T}"/>.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="handlers"></param>
-        /// <returns></returns>
+        /// <typeparam name="T">The type of <see cref="System.EventArgs"/> used by the handler.</typeparam>
+        /// <param name="handlers">A sequence of <see cref="System.EventHandler{T}"/></param>
+        /// <returns>A multicast <see cref="System.EventHandler{T}"/> with an invocation list of handlers.</returns>
         public static EventHandler<T> Combine<T>(this IEnumerable<EventHandler<T>> handlers) where T : EventArgs
         {
-            return handlers.Aggregate((EventHandler<T>)null, (result, h) => result += h);
+            return handlers.Aggregate((EventHandler<T>)null, (result, h) => result + h);
         }
 
         /// <summary>
